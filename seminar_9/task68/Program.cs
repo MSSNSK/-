5d49f88f2ed0c5ed1ec09883abf0c663d7a874a0
Markov Sergei <irbis_mssw@mail.ru>
2022-09-17 19:35:21 +0700

    Fix task

--- seminar_9/task68/Program.cs
+++ seminar_9/task68/Program.cs
@@ -6,9 +6,9 @@
 int AkkermanFunction(int m, int n)
 {
     if (m == 0) return n + 1;
-    else if (m > 0 & n == 0) return AkkermanFunction(m-1, 1);
+    else if (m > 0 & n == 0) return AkkermanFunction(m - 1, 1);
     else if (m > 0 & n > 0)
-    return AkkermanFunction(m-1, AkkermanFunction(m, n-1));
+    return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
     return 0;
 }
 
